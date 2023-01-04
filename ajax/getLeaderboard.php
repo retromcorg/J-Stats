051477093b0f5166dce92504ed0bfcbf7215a5b1
sui <abandon3d@protonmail.com>
2023-01-04 10:33:35 -0800

    pushed 1672857215

--- ajax/getLeaderboard.php
+++ ajax/getLeaderboard.php
@@ -53,15 +53,5 @@ foreach ($d as $row) {
 }
 }
 }
-    function super_unique($array,$key)
-	        {
-			       $temp_array = [];
-			              foreach ($array as &$v) {
-					                 if (!isset($temp_array[$v[$key]]))
-								            $temp_array[$v[$key]] =& $v;
-							        }
-			              $array = array_values($temp_array);
-			              return $array;
 
-				          }
 ?>
