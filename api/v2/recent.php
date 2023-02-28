46a080c59a9e6bcc275951f1367914d021f683e1
sui <abandon3d@protonmail.com>
2023-02-28 12:14:09 -0800

    pushed 1677615249

---
+++ api/v2/recent.php
@@ -0,0 +1,36 @@
+<?php
+
+
+include_once("../../internal/backbone.php");
+
+header('Content-type: application/json; charset=utf-8');
+
+
+$builder = [];
+
+$db->orderby("firstJoin", "DESC");
+$added = $db->get("users", 5);
+
+foreach($added as $k => $a) {
+    $builder['recently_added'][] = [
+        "username" => $a['username'],
+        "uuid" => $a['uuid'],
+        "date" => $a['firstJoin']
+    ];
+}
+
+$db->orderby("lastJoin", "DESC");
+$updated = $db->get("users", 5);
+
+foreach($updated as $k => $u) {
+    $builder['recently_updated'][] = [
+        "username" => $a['username'],
+        "uuid" => $a['uuid'],
+        "date" => $a['lastJoin']
+    ];
+}
+
+
+echo json_encode($builder, true);
+
+?>
